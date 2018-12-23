9834ed88b182b5e01ffa62879503f0c1b8a43a8f
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2018-12-23 14:33:33 +0100

    Add PRO file

---
+++ src/addHoursAndMinutes.pro
@@ -0,0 +1,10 @@
+QT += qml quick
+  
+CONFIG += c++11
+
+RESOURCES += addHoursAndMinutes.qrc
+RESOURCES += addHoursAndMinutes_translations.qrc 
+
+SOURCES += main.cpp
+
+TRANSLATIONS = addHoursAndMinutes_de.ts
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include <QApplication>
 #include <QGuiApplication>
 #include <QFile>
 #include <QFont>
@@ -31,7 +30,7 @@
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
+  QGuiApplication app(argc, argv);
   
   // Install translators
   QString locale = QLocale::system().name();
