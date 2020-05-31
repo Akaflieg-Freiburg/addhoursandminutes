9e624ceea9ded18bab710d2f5953c8c1dfa4c328
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2020-05-31 17:22:25 +0200

    Work on webasm build script

---
+++ buildscript-webasm.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#
+# This script builds "Add Hours and minutes" for webasm in release mode.
+#
+# See https://github.com/Akaflieg-Freiburg/addhoursandminutes/wiki/Build-scripts
+#
+
+
+#
+# Copyright © 2020 Stefan Kebekus <stefan.kebekus@math.uni-freiburg.de>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+
+#
+# Fail on first error
+#
+
+set -e
+
+
+#
+# Clean up
+#
+
+rm -rf build-webasm-release
+mkdir -p build-webasm-release
+cd build-webasm-release
+
+#
+# Configure
+#
+
+. $EMSDK/emsdk_env.sh
+$Qt5_DIR_WASM/bin/qmake ../src/addhoursandminutes.pro
+
+
+#
+# Build the executable
+#
+
+$Qt5_DIR_WASM/bin/lrelease ../src/addhoursandminutes.pro
+make -j
+cp ../metadata/de.akaflieg_freiburg.cavok.add_hours_and_minutes.svg qtlogo.svg
--- src/addhoursandminutes.pro
+++ src/addhoursandminutes.pro
@@ -1,31 +1,15 @@
-android: QT += androidextras
 QT += qml
 QT += quick
+  
+CONFIG += qtquickcompiler
 CONFIG += c++11
+CONFIG += release
 
-# The following define makes your compiler emit warnings if you use
-# any Qt feature that has been marked deprecated (the exact warnings
-# depend on your compiler). Refer to the documentation for the
-# deprecated API to know how to port your code APIsaway from it.
-DEFINES += QT_DEPRECATED_WARNINGS
-
-# You can also make your code fail to compile if it uses deprecated APIs.
-# In order to do so, uncomment the following line.
-# You can also select to disable deprecated APIs only up to a certain version of Qt.
-DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the  deprecated before Qt 6.0.0
-
-SOURCES += androidAdaptor.cpp \
-main.cpp
+SOURCES += androidAdaptor.cpp 
+SOURCES += main.cpp
 
 HEADERS += androidAdaptor.h
 
 RESOURCES += addhoursandminutes.qrc
 
 TRANSLATIONS += addhoursandminutes_de.ts
-
-# Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
-!isEmpty(target.path): INSTALLS += target
-
-ANDROID_PACKAGE_SOURCE_DIR = $$PWD/android-sources
