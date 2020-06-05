d1a17c71c2d348d7815096e6bc7064fd79f8c539
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2020-06-05 22:43:21 +0200

    Make it compile under Android

---
+++ buildscript-linux-debug.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# This script builds "enroute flight navigation" for the Linux desktop in
+# "Debug" mode.  Several sanitizers are switched on.
+#
+# See https://github.com/Akaflieg-Freiburg/enroute/wiki/Build-scripts
+#
+
+#
+# Copyright © 2020 Stefan Kebekus <stefan.kebekus@math.uni-freiburg.de>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#
+# Run this script in the main directory tree.
+
+#
+# Clean
+#
+
+rm -rf build-linux-debug
+
+#
+# Build the executable
+#
+
+mkdir build-linux-debug
+cd build-linux-debug
+
+export ASAN_OPTIONS=detect_leaks=0
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+cmake \
+    -G Ninja\
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_C_FLAGS="-fsanitize=address -fsanitize=undefined" \
+    -DCMAKE_CXX_FLAGS="-fsanitize=address -fsanitize=undefined -Werror -Wall -Wextra" \
+    -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address -fsanitize=undefined" \
+    -DCMAKE_INSTALL_PREFIX=/ \
+    -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address -fsanitize=undefined" \
+    -DCMAKE_FIND_ROOT_PATH:STRING=$Qt5_DIR_LINUX \
+    ..
+
+ninja
+cd ..
