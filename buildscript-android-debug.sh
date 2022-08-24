6060594fe4508e117963b30a69e36d3905150155
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2022-08-24 10:43:11 +0200

    Startup

--- buildscript-android-debug.sh
+++ buildscript-android-debug.sh
@@ -48,8 +48,9 @@ cd build-android-debug
 ANDROID_NDK_ROOT=$ANDROID_SDK_ROOT/ndk/22.1.7171670
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.15.0.10-1.fc35.x86_64
 
-$Qt6_DIR_ANDROID\_x86/bin/qt-cmake .. \
+$Qt6_DIR_ANDROID\_armv7/bin/qt-cmake .. \
   -G Ninja \
+  -DQT_ANDROID_BUILD_ALL_ABIS=ON \
   -DCMAKE_BUILD_TYPE:STRING=Debug
 
 
