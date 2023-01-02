32fd47ef71bf498ef4e0922ed92fb997b3c17b89
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-01-02 08:53:06 +0100

    New webassembly

---
+++ publish-webasm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build for webassembly
+./buildscript-webasm.sh
+
+
+#
+# Distribute the webassembly executable
+#
+
+echo "Installing webassembly files…"
+cd build-webasm-release
+cp qtloader.js qtlogo.png addhoursandminutes.html addhoursandminutes.js addhoursandminutes.wasm ../docs/assets/webasm/
+cd ..
+git commit -am "New webassembly"
