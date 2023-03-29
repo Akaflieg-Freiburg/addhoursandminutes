12e4f386a58dde9a3726398dffbedd6112c55062
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-03-29 09:53:23 +0200

    Minor cleanup

--- buildscript-android-release.sh
+++ buildscript-android-release.sh
@@ -44,7 +44,7 @@ cd build-android-release
 #
 
 export ANDROID_NDK_ROOT=$ANDROID_SDK_ROOT/ndk/23.1.7779620
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-17.0.5.0.8-1.fc37.x86_64
+export JAVA_HOME=/usr/lib/jvm/java-19-openjdk
 
 $Qt6_DIR_ANDROID\_x86/bin/qt-cmake .. \
   -G Ninja\
